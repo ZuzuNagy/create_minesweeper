6624231b66bd20d1e25acf5653b346fa8268f1c1
Nagy Zsuzsanna <nagy.zsuzsanna@ejogseged.hu>
2019-01-22 16:52:38 +0100

    Create hozzáadása

--- app/table.rb
+++ app/table.rb
@@ -41,11 +41,41 @@ class Table
     "\n#{table}"
   end
 
-  def each_field_around x, y, &block
-    [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]].each do |(x_dir,y_dir)|
+  def each_coordinate_around x, y
+    directions = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+    coordinates_if_in_table = proc do |x,y,x_dir,y_dir, &block|
       x1 = x + x_dir
       y1 = y + y_dir
-      block.call([x1, y1]) if x1 >= 0 && y1 >= 0 && x1 < @rows && y1 < @cols
+      block.([x1,y1]) if x1 >= 0 && y1 >= 0 && x1 < @rows && y1 < @cols
+    end
+
+    if block_given?
+      directions.inject([]) do |coordinates, (x_dir, y_dir)|
+        coordinates_if_in_table.call(x,y,x_dir,y_dir) do |coordinate|
+          coordinates << coordinate
+          yield(coordinate)
+        end
+        coordinates
+      end
+      #  directions.inject([]) do |coordinates, (x_dir,y_dir)|
+      #    coordinate, in_table = coordinates_and_is_in_table(x,y,x_dir,y_dir)
+      #    if in_table
+      #      coordinates << coordinate
+      #      yield(coordinate)
+      #    end
+      #    coordinates
+      #  end
+    else
+      Enumerator.new do |yielder|
+        directions.each do |(x_dir, y_dir)|
+          coordinates_if_in_table.call(x,y,x_dir,y_dir) { |coordinate| yielder << coordinate }
+        end
+      end
+      #    directions.each do |(x_dir,y_dir)|
+      #      coordinate, in_table = coordinates_and_is_in_table(x,y,x_dir,y_dir)
+      #      yielder << coordinate if in_table
+      #    end
+      #  end
     end
   end
 
@@ -66,7 +96,7 @@ class Table
 
   def mines_count_around x,y
     count = 0
-    each_field_around(x,y) do |coordinate|
+    each_coordinate_around(x,y) do |coordinate|
       count += 1 if @mine_coordinates.include?(coordinate)
     end
     count
@@ -74,7 +104,7 @@ class Table
 
   def marked_count_around x,y
     marked_count = 0
-    each_field_around(x,y) do |coordinate|
+    each_coordinate_around(x,y) do |coordinate|
       marked_count += 1 if self[*coordinate].marked?
     end
     marked_count
@@ -82,18 +112,29 @@ class Table
 
   def pick_around x,y
     if self[x,y].value&.<= marked_count_around(x,y)
-      each_field_around(x,y) { |coordinate| pick(*coordinate) }
+      each_coordinate_around(x,y) { |coordinate| pick(*coordinate) }
     end
     self
   end
 
-#  def all_untouched_field_in_the_table x,y
-#
-#  end
-#
-#  def picked_mine x,y
-#    if self[x,y].picked? && self[x,y].value == 'x'
-#
-#    end
-#  end
+  def coordinates_and_is_in_table x, y, x_dir, y_dir
+    x1 = x+x_dir
+    y1 = y+y_dir
+    [[x1,y1], x1 >= 0 && y1 >= 0 && x1 < @rows && y1 < @cols]
+  end
+
+  def self.create rows, cols, mines_count
+    mine_coordinates = generate_grid(rows, cols).sample(mines_count)
+      new(rows, cols, mine_coordinates)
+  end
+
+  def self.generate_grid rows, cols
+    (0...rows).inject([]) do |coor, x|
+      (0...cols).each do |y|
+        coor << [x,y]
+      end
+      coor
+    end
+  end
+
 end
